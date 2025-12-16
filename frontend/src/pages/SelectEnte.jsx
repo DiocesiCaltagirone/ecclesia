ecc2bc757d0861339260a7cbc0162a0805852d97
Luciano Di Silvestro <economatocaltagirone@gmail.com>
2025-12-16 13:12:37 +0100

    SelectEnte: testo aggiornato

--- frontend/src/pages/SelectEnte.jsx
+++ frontend/src/pages/SelectEnte.jsx
@@ -76,10 +76,10 @@ function SelectEnte() {
           <div className="flex justify-between items-center mb-8">
             <div>
               <h1 className="text-3xl font-bold text-gray-800 mb-2">
-                Seleziona Parrocchia
+                ENTE
               </h1>
               <p className="text-gray-600">
-                Scegli la parrocchia su cui lavorare
+                Scegli l'ente su cui lavorare
               </p>
             </div>
             
